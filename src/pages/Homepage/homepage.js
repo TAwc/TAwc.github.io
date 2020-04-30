64717beae785aafe08c2d5c7eaa71e3f85410948
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-04-30 19:24:55 -0400

    Updated home to have links

--- src/pages/Homepage/homepage.js
+++ src/pages/Homepage/homepage.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import styles from './homepage.module.css'
+import {Link} from 'react-router-dom'
 
 
 
 const Homepage = () =>  {
-  
-    //const [petTypes, setPetTypes] = useState([]);
-    
-    /*useEffect(() => {
-      pf.animalData.types()
-      .then(resp => {
-        setPetTypes(resp.data.types);
-      });
-    }, []);*/
-   
+
     return (
       <div>
           <h1 className={styles.h1}>Hello</h1>
+          <ul>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/Projects">Projects</Link>
+          </ul>
       </div>
     );
 }
