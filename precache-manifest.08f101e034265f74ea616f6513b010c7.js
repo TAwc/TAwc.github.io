500b2c28f9eb9e7bb6e2baa2f62222746de8eafd
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-04-30 17:33:14 -0400

    Updates

---
+++ precache-manifest.08f101e034265f74ea616f6513b010c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d758d94b381a72c7429e7ed7e49d710",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "2b5215510631353f85c1",
+    "url": "/website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd25bf603745ea560b61",
+    "url": "/website/static/js/2.c71f1797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.c71f1797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5215510631353f85c1",
+    "url": "/website/static/js/main.63c18e33.chunk.js"
+  },
+  {
+    "revision": "ba2382361bea94564ff1",
+    "url": "/website/static/js/runtime-main.e06ec2de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
