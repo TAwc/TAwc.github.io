7ffeb4f7f9d6e73f6cf49003f8d122af8da169bc
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-05-04 15:54:04 -0400

    grammer summary

--- src/components/Homepage/Summary/summary.js
+++ src/components/Homepage/Summary/summary.js
@@ -28,11 +28,11 @@ const Header = () =>  {
                   </Col>
                   <Col lg={5}>
                     <p className={styles.sumtext}>
-                      My name is Alexander Waters, A Computer scince and innovation major at Champlain College scheduled to get my degree in May 2020.
-                      while excited for almost anything about programming, one of my biggest intrest lies within Artificial intelligence. With Skills I
-                      Have built via interships, jobs, classes, and most importantly peronsal projects, Im excited to tackel the next chapter and challenge 
-                      of my life. While programming is a big part of my life, I have other hobbies such as photogrpahy and photo/video edditing. Click 
-                      around to find out more 
+                    My name is Alexander Waters, A Computer since and innovation major at Champlain College scheduled to get my degree in May 2020. 
+                    while excited for almost anything about programming, one of my biggest interest lies within Artificial intelligence. 
+                    With skills I have built via an internship, jobs, classes, and most importantly personal projects, I'm excited to tackle the next 
+                    chapter and challenge in my life. While programming is a big part of my life, I have other hobbies such as photography and photo/video 
+                    editing. Click around to find out more
                     </p>
                   </Col>
                   <Col sm={2}>
