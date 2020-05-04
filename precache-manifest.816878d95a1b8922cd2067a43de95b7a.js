bb88b3f67dd3814228bcb85011ff3638b0979c62
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-05-03 22:18:13 -0400

    Updates

---
+++ precache-manifest.816878d95a1b8922cd2067a43de95b7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1964c3ae9072632a7f1cf46380410c8f",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "060801ffafcdb7df07c5",
+    "url": "/website/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9c748758310b3934249e",
+    "url": "/website/static/css/main.ce3710fd.chunk.css"
+  },
+  {
+    "revision": "060801ffafcdb7df07c5",
+    "url": "/website/static/js/2.b6dad4c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.b6dad4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c748758310b3934249e",
+    "url": "/website/static/js/main.2c0e4386.chunk.js"
+  },
+  {
+    "revision": "ba2382361bea94564ff1",
+    "url": "/website/static/js/runtime-main.e06ec2de.js"
+  },
+  {
+    "revision": "f01a3bf234bba6adc54aa1474708595a",
+    "url": "/website/static/media/Alex_pic.f01a3bf2.png"
+  },
+  {
+    "revision": "44f28ff5ddf738f8add1df097a7cb4c1",
+    "url": "/website/static/media/ReadMoreArrow.44f28ff5.png"
+  }
+]);
