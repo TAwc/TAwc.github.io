eda3535ea4764f3af4d865fff4537f5a377b44a9
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2021-03-23 16:48:06 -0400

    fixed broken repo link

--- src/components/Projects/Current/current.js
+++ src/components/Projects/Current/current.js
@@ -30,7 +30,7 @@ const Future = () =>  {
   const projects = [ 
     [ 
     {name : "Vector Library", description : "A Vector Library a friend an I are building, aiming for performance", pic : NA, link : "https://github.com/trobol/dak_math#", techs : ["C++"]}, 
-    {name : "Chess Engine", description : "A chess engine using the Minimax Algorithm to determine the best moves", pic : NA, link : "", techs : ["Chess","Multi-threading","C++","Python"]}],
+    {name : "Chess Engine", description : "A chess engine using the Minimax Algorithm to determine the best moves", pic : NA, link : "https://github.com/TAwc/ChessEngine", techs : ["Chess","Multi-threading","C++","Python"]}],
     ]
 
 
