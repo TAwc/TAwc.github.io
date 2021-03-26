930fe660f2a7f54a2494921fb5d88b4508a46b04
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2021-03-26 14:47:26 -0400

    Updates

---
+++ precache-manifest.e3794025af624dbcbb89689683ad0e33.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbfdc3f881369e05cc4fc4aa8239fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3b4d5e4e5b85183bad",
+    "url": "/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "15b673c1bf453f586536",
+    "url": "/static/css/main.9908c852.chunk.css"
+  },
+  {
+    "revision": "0c3b4d5e4e5b85183bad",
+    "url": "/static/js/2.44aa26e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.44aa26e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b673c1bf453f586536",
+    "url": "/static/js/main.0261a5f3.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "063b0ffa8e55c04649a410aeef0b72bd",
+    "url": "/static/media/A-Star..063b0ffa.PNG"
+  },
+  {
+    "revision": "f313f9de007710f2f405ef1cec547618",
+    "url": "/static/media/Chat.f313f9de.PNG"
+  },
+  {
+    "revision": "2bdd175d810a24b5376219e6700c0a79",
+    "url": "/static/media/Corona.2bdd175d.PNG"
+  },
+  {
+    "revision": "4cc85930faef0c7127b58ace9b747811",
+    "url": "/static/media/Headshot 5-4-2020.4cc85930.JPG"
+  },
+  {
+    "revision": "27083a2ad867b825b837d6f32ad59bb3",
+    "url": "/static/media/Headshot(edit) 5-4-2020.27083a2a.jpg"
+  },
+  {
+    "revision": "a58ea054bb4c15289e75b15d48c489f7",
+    "url": "/static/media/Hiking.a58ea054.jpg"
+  },
+  {
+    "revision": "80bdabdc8254b166414c7ea87385be9e",
+    "url": "/static/media/Leaves.80bdabdc.JPG"
+  },
+  {
+    "revision": "d57e16038ca36e176d77aaaf02aff716",
+    "url": "/static/media/Montreal.d57e1603.JPG"
+  },
+  {
+    "revision": "f231857bcad743474519bb2c1dc2ddc2",
+    "url": "/static/media/N-A.f231857b.PNG"
+  },
+  {
+    "revision": "2babaa06493cc32648e7e8fca755c63b",
+    "url": "/static/media/NN.2babaa06.PNG"
+  },
+  {
+    "revision": "44f28ff5ddf738f8add1df097a7cb4c1",
+    "url": "/static/media/ReadMoreArrow.44f28ff5.png"
+  },
+  {
+    "revision": "380c1109690a659e388d5accf9fc4dad",
+    "url": "/static/media/Website.380c1109.PNG"
+  },
+  {
+    "revision": "efacba2c79c4af41e57c20518de770da",
+    "url": "/static/media/snake.efacba2c.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.3947aa27395b7c9438361dc467650da7.js"
+  "/precache-manifest.e3794025af624dbcbb89689683ad0e33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
