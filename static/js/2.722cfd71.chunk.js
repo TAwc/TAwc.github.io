23e043728ad60255f88f2c691e0bc9b98cdb44a5
Alexander Waters <AlexanderWatermenAWC@gmail.com>
2021-12-22 13:01:39 -0500

    Updates

--- static/js/2.722cfd71.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[2],{81:function(e,t,n){"use strict";n.r(t);var s=n(0),a=n.n(s);t.default=function(){return a.a.createElement("div",null,a.a.createElement("p",null,"I AM POST 2"))}}}]);
-//# sourceMappingURL=2.722cfd71.chunk.js.map
